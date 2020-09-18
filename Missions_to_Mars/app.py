32d86ffcadc3ed495d76fe14e1b21eee271efc58
Jayden LeBlanc <jaydenleblanc@gmail.com>
2020-09-17 23:39:19 -0400

    Flask Application

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -2,3 +2,28 @@ from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
 import scrape_mars
 
+app = Flask(__name__)
+
+#Use flask_pymongo to set up mongodb connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_hemispheres_app"
+mongo = PyMongo(app)
+
+#This is the route that will be used for the home page from Mongo
+@app.route("/")
+def index():
+    listings = mongo.db.collection.find_one()
+    return render_template("index.html", listings=listings)
+
+
+#This is the route that will trigger the scrape function
+@app.route("/scrape")
+def scrapper():
+    
+    listings_data = scrape_mars.scrape()
+    mongo.db.collection.update({}, listings_data, upsert=True)
+    return redirect("/", code=302)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # web-scraping-challenge
+
+In this challenge a web application was built by scraping various websites to present on a single HTML page. Data was gathered in Jupyter Notebooks and then converted into a python script in order to connect to Mongo DB and create a Flask application. 
